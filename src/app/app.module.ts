a3dd33f916d5aa7eea502c9bcb36603914fbde71
HaszonHegedusEszter <hegedus.eszter85@gmail.com>
2021-04-29 14:30:13 +0200

    create starter templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogListComponent } from './components/blog-list/blog-list.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogListComponent,
+    BlogDetailsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
